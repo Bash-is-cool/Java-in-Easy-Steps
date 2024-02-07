7c6509492c6d9a0d31a3a380ae773bd7a38fc31c
fufki <coolddg@duck.com>
2024-02-06 23:26:14 -0600

    Java in easy steps

--- src/Chapter 3.java
+++ src/Chapter 3.java
@@ -12,3 +12,111 @@ class If
         );
     }
 }
+class Else
+{
+    public static void main(String[] args) {
+        int hrs = 15;
+        if (hrs < 13)
+        {
+            System.out.println("Good Morning: " + hrs);
+        } else if (hrs < 18) {
+            System.out.println("Good Afternoon: " + hrs);
+        }
+    }
+}
+
+class Switch
+{
+    public static void main(String[] args) {
+        int month = 2, year = 2024, num =31;
+        switch ( month)
+        {
+            case 4 : case 6 : case 9 : case 11 : num = 30; break;
+            case 2 : num = (year % 4 == 0) ? 29 : 28; break;
+        }
+        System.out.println(month + "/" + year + "/" + num + "days");
+    }
+}
+
+class For
+{
+    public static void main(String[] args) {
+        int num = 0;
+        for (int i = 1; i <4; i++)
+        {
+            System.out.println("Outer Loop i = " + i);
+            for ( int j = 1; j < 4; j++)
+            {
+                System.out.print("\tInner Loop j = " + j);
+                System.out.println("\t\tTotal num = " + (++ num));
+            }
+        }
+    }
+}
+
+class While
+{
+    public static void main(String[] args) {
+        int num = 100;
+        while (num > 0)
+        {
+            System.out.println("While Countdown: " + num);
+            num -= 10;
+        }
+    }
+}
+
+class DoWhile
+{
+    public static void main(String[] args) {
+        int num = 100;
+        do
+        {
+            System.out.println("DoWhile Countup: " + num);
+            num += 10;
+        }
+        while (num < 10);
+    }
+}
+
+class Break
+{
+    public static void main(String[] args) {
+        for ( int i = 1; i < 4; i++)
+        {
+            for (int j =1; j < 4; j++)
+            {
+                if (i == 1 && j == 1)
+                {
+                    System.out.println("Continues innerLoop when i = " + i + " j = " + j);
+                    continue;
+                }
+                if ( i == 2 && j == 1)
+                {
+                    System.out.println("Breaks innerLoop when i = " + i + " j = " + j);
+                    break;
+                }
+                System.out.println("Running i = " + i + " j = " + j);
+            }
+        }
+    }
+}
+
+class Label
+{
+    public static void main(String[] args) {
+       outerLoop : for (int i = 1; i < 4; i++)
+        {
+            for (int j = 1; j < 4; j++)
+            {
+                System.out.println("Running i = " + i + " j = " + j);
+                if (i == 1 && j == 1)
+                {
+                    System.out.println("Continues outerLoop when i = " + i + "j = " + j);
+                    continue outerLoop;
+                    //Works for the break statement too
+                }
+            }
+        }
+    }
+}
