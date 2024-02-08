2be477e4d5af79d9ac14ff19a6af7cd286687aa8
fufki <coolddg@duck.com>
2024-02-07 20:11:44 -0600

    Java in easy steps

--- src/Chapter4.java
+++ src/Chapter4.java
@@ -3,8 +3,8 @@ class convert
     public static void main(String[] args) {
         float daysFloat = 365.25f;
         String weeksString = "52";
-        int weeksInt = (int) daysFloat;
-        int weeksInt = Integer.parselInt(weeksString);
+        int daysInt = (int) daysFloat;
+        int weeksInt = Integer.parseInt(weeksString);
         int week = (daysInt / weeksInt);
         System.out.println("Days in a week: " + week);
     }
